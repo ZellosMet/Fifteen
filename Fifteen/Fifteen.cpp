2655c003756b68caa1c0c836420b0fb7ef9d7f0e
Professional <Professional@USER-PC>
2023-04-01 15:57:11 +0900

    DONE

--- Fifteen/Fifteen.cpp
+++ Fifteen/Fifteen.cpp
@@ -3,7 +3,7 @@
 #include<time.h>
 using namespace std;
 
-const int SIZE1 = 4;
+const int SIZE = 4;
 
 #define MOVE_LEFT 75  //Задание переменных для отлова с клавиатуры
 #define MOVE_RIGHT 77
@@ -11,51 +11,54 @@ const int SIZE1 = 4;
 #define MOVE_DOWN 80
 #define ESC 27
 
-void Fill_Grid(int grid[SIZE1][SIZE1], int SIZE1);
-void Print_Grid(int grid[SIZE1][SIZE1], int SIZE1);
-void Move_Up(int grid[SIZE1][SIZE1], int SIZE1, int *pemp_i, int* pemp_j);
-void Move_Down(int grid[SIZE1][SIZE1], int SIZE1, int *pemp_i, int* pemp_j);
-void Move_Right(int grid[SIZE1][SIZE1], int SIZE1, int *pemp_i, int* pemp_j);
-void Move_Left(int grid[SIZE1][SIZE1], int SIZE1, int *pemp_i, int* pemp_j);
-void Mix_Grid(int grid[SIZE1][SIZE1], int arr_move[SIZE1], int SIZE1, int* pemp_i, int *pemp_j);
+void Fill_Grid(int grid[SIZE][SIZE], int SIZE); //Функция заполнения сетки
+void Print_Grid(int grid[SIZE][SIZE], int SIZE); // Функция вывода сетки
+void Move_Up(int grid[SIZE][SIZE], int SIZE, int *pemp_i, int* pemp_j); //Функции перемещения пустого блока
+void Move_Down(int grid[SIZE][SIZE], int SIZE, int *pemp_i, int* pemp_j);
+void Move_Right(int grid[SIZE][SIZE], int SIZE, int *pemp_i, int* pemp_j);
+void Move_Left(int grid[SIZE][SIZE], int SIZE, int *pemp_i, int* pemp_j);
+void Mix_Grid(int grid[SIZE][SIZE], int arr_move[SIZE], int SIZE, int* pemp_i, int *pemp_j); //Функция перемешивания сеетки
+int Check_Win(int grid[SIZE][SIZE], int SIZE, int control); //Функция проерки сетки на попеду
 
 void main()
 {
 	srand(time(NULL));
+	setlocale(LC_ALL, "ru");
 
-	int grid[SIZE1][SIZE1] = {};
-	int arr_move[SIZE1] = { MOVE_UP, MOVE_LEFT, MOVE_RIGHT, MOVE_DOWN };
-	int control;
-	int emp_i, emp_j;
-	emp_i = emp_j = SIZE1 - 1;
+	int grid[SIZE][SIZE] = {};
+	int arr_move[SIZE] = { MOVE_UP, MOVE_LEFT, MOVE_RIGHT, MOVE_DOWN };
+	int control, emp_i, emp_j;
+	emp_i = emp_j = SIZE - 1;
 
 	int *pemp_i, *pemp_j;
 	pemp_i = &emp_i;
 	pemp_j = &emp_j;
 
-	Fill_Grid(grid, SIZE1);
-	Mix_Grid(grid, arr_move, SIZE1, pemp_i, pemp_j);
-	Print_Grid(grid, SIZE1);
+	Fill_Grid(grid, SIZE);
+	Mix_Grid(grid, arr_move, SIZE, pemp_i, pemp_j);
+	Print_Grid(grid, SIZE);
+	cout << "\nПравила: Необходимао расположить числа в порядке возрастания начиная с верхнего левого угла.";
+	cout << "\nПеремещение числе осуществляется перемещением пустого квадрата стрелочками.";	
 
 	do //Цикл отлова событий
 	{
 		control = _getch();
 		switch (control)
 		{
-		case MOVE_UP: Move_Up(grid, SIZE1, pemp_i, pemp_j); Print_Grid(grid, SIZE1); break;
-		case MOVE_DOWN: Move_Down(grid, SIZE1, pemp_i, pemp_j); Print_Grid(grid, SIZE1); break;
-		case MOVE_RIGHT: Move_Right(grid, SIZE1, pemp_i, pemp_j); Print_Grid(grid, SIZE1); break;
-		case MOVE_LEFT: Move_Left(grid, SIZE1, pemp_i, pemp_j); Print_Grid(grid, SIZE1); break;
+		case MOVE_UP: Move_Up(grid, SIZE, pemp_i, pemp_j); Print_Grid(grid, SIZE); control = Check_Win(grid, SIZE, control); break;
+		case MOVE_DOWN: Move_Down(grid, SIZE, pemp_i, pemp_j); Print_Grid(grid, SIZE); control = Check_Win(grid, SIZE, control); break;
+		case MOVE_RIGHT: Move_Right(grid, SIZE, pemp_i, pemp_j); Print_Grid(grid, SIZE); control = Check_Win(grid, SIZE, control); break;
+		case MOVE_LEFT: Move_Left(grid, SIZE, pemp_i, pemp_j); Print_Grid(grid, SIZE); control = Check_Win(grid, SIZE, control); break;
 		}
 	} while (control != ESC);
 }
 
-void Fill_Grid(int grid[SIZE1][SIZE1], int SIZE1)
+void Fill_Grid(int grid[SIZE][SIZE], int SIZE)
 {
 	int cnt = 1;
-	for (int i = 0; i < SIZE1; i++)
+	for (int i = 0; i < SIZE; i++)
 	{
-		for (int j = 0; j < (i==SIZE1-1? SIZE1-1 : SIZE1); j++)
+		for (int j = 0; j < (i==SIZE-1? SIZE-1 : SIZE); j++)
 		{
 			grid[i][j] = cnt;
 			cnt++;			
@@ -63,31 +66,22 @@ void Fill_Grid(int grid[SIZE1][SIZE1], int SIZE1)
 	}
 }
 
-void Print_Grid(int grid[SIZE1][SIZE1], int SIZE1)
+void Print_Grid(int grid[SIZE][SIZE], int SIZE)
 {
 	system("cls");
-	for (int i = 0; i < SIZE1; i++)
+	for (int i = 0; i < SIZE; i++)
 	{
-		for (int j = 0; j < SIZE1; j++)
+		for (int j = 0; j < SIZE; j++)
 		{
-			if (grid[i][j] == 0)
-			{
-				cout << "[    ] ";
-			}
-			else if (grid[i][j]<10)
-			{
-				cout << "[  " << grid[i][j] << " ] ";
-			}
-			else if (grid[i][j] >= 10)
-			{
-				cout << "[ " << grid[i][j] << " ] ";
-			}
+			if (grid[i][j] == 0) cout << "[    ] ";
+			else if (grid[i][j]<10) cout << "[  " << grid[i][j] << " ] ";
+			else if (grid[i][j] >= 10) cout << "[ " << grid[i][j] << " ] ";
 		}
 		cout << endl;
 	}
 }
 
-void Move_Up(int grid[SIZE1][SIZE1], int SIZE1, int* pemp_i, int* pemp_j)
+void Move_Up(int grid[SIZE][SIZE], int SIZE, int* pemp_i, int* pemp_j)
 {
 	if (*pemp_i != 0)
 	{
@@ -97,9 +91,9 @@ void Move_Up(int grid[SIZE1][SIZE1], int SIZE1, int* pemp_i, int* pemp_j)
 		*pemp_i = *pemp_i - 1;
 	}
 }
-void Move_Down(int grid[SIZE1][SIZE1], int SIZE1, int* pemp_i, int* pemp_j)
+void Move_Down(int grid[SIZE][SIZE], int SIZE, int* pemp_i, int* pemp_j)
 {
-	if (*pemp_i != SIZE1 - 1)
+	if (*pemp_i != SIZE - 1)
 	{
 		grid[(*pemp_i) + 1][*pemp_j] ^= grid[*pemp_i][*pemp_j];
 		grid[*pemp_i][*pemp_j] ^= grid[(*pemp_i) + 1][*pemp_j];
@@ -107,9 +101,9 @@ void Move_Down(int grid[SIZE1][SIZE1], int SIZE1, int* pemp_i, int* pemp_j)
 		*pemp_i = *pemp_i + 1;
 	}
 }
-void Move_Right(int grid[SIZE1][SIZE1], int SIZE1, int* pemp_i, int* pemp_j)
+void Move_Right(int grid[SIZE][SIZE], int SIZE, int* pemp_i, int* pemp_j)
 {
-	if (*pemp_j != SIZE1 - 1)
+	if (*pemp_j != SIZE - 1)
 	{
 		grid[*pemp_i][(*pemp_j) + 1] ^= grid[*pemp_i][*pemp_j];
 		grid[*pemp_i][*pemp_j] ^= grid[*pemp_i][(*pemp_j) + 1];
@@ -117,7 +111,7 @@ void Move_Right(int grid[SIZE1][SIZE1], int SIZE1, int* pemp_i, int* pemp_j)
 		*pemp_j = *pemp_j + 1;
 	}
 }
-void Move_Left(int grid[SIZE1][SIZE1], int SIZE1, int* pemp_i, int* pemp_j)
+void Move_Left(int grid[SIZE][SIZE], int SIZE, int* pemp_i, int* pemp_j)
 {
 	if (*pemp_j != 0)
 	{
@@ -128,20 +122,35 @@ void Move_Left(int grid[SIZE1][SIZE1], int SIZE1, int* pemp_i, int* pemp_j)
 	}
 }
 
-void Mix_Grid(int grid[SIZE1][SIZE1], int arr_move[SIZE1], int SIZE1, int *pemp_i, int *pemp_j)
+void Mix_Grid(int grid[SIZE][SIZE], int arr_move[SIZE], int SIZE, int *pemp_i, int *pemp_j)
 {
-	int cntrl, old_cntrl = 5;
-	for (int i = 0; i <= 100; )
+	int cntrl, old_cntrl = SIZE;
+	for (int i = 0; i < 100; )
 	{
 		int cntrl = rand() % 4;
 		if (cntrl != old_cntrl)
 		{
-			if (arr_move[cntrl] == MOVE_UP) Move_Up(grid, SIZE1, pemp_i, pemp_j);
-			else if (arr_move[cntrl] == MOVE_DOWN) Move_Down(grid, SIZE1, pemp_i, pemp_j);
-			else if (arr_move[cntrl] == MOVE_RIGHT) Move_Right(grid, SIZE1, pemp_i, pemp_j);
-			else if (arr_move[cntrl] == MOVE_LEFT) Move_Left(grid, SIZE1, pemp_i, pemp_j);
+			if (arr_move[cntrl] == MOVE_UP) Move_Up(grid, SIZE, pemp_i, pemp_j);
+			else if (arr_move[cntrl] == MOVE_DOWN) Move_Down(grid, SIZE, pemp_i, pemp_j);
+			else if (arr_move[cntrl] == MOVE_RIGHT) Move_Right(grid, SIZE, pemp_i, pemp_j);
+			else if (arr_move[cntrl] == MOVE_LEFT) Move_Left(grid, SIZE, pemp_i, pemp_j);
 			i++;
 		}
 		old_cntrl = cntrl;
 	}
+}
+
+int Check_Win(int grid[SIZE][SIZE], int SIZE, int control)
+{
+	int check = 1;
+	for (int i = 0; i < SIZE; i++)
+	{
+		for (int j = 0; j < (i == SIZE - 1 ? SIZE - 1 : SIZE); j++)
+		{
+			if (grid[i][j] == check) check++;
+			else return 0;
+		}
+	}
+	cout << "\n\t Победа!\n"; 
+	return control = ESC;
 }
